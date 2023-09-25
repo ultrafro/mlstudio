f925cbd1ccc2ed0e3183226316dfcb275f38752b
ultrafro <hisham.bedri@gmail.com>
2023-09-25 01:34:38 -0400

    add react flow

--- app/GraphDisplay.tsx
+++ app/GraphDisplay.tsx
@@ -1,6 +1,31 @@
+import { useCallback } from "react";
 import { FlexCol, FlexRow } from "./Flex";
+import ReactFlow, {
+  useEdgesState,
+  useNodesState,
+  addEdge,
+  Controls,
+  MiniMap,
+  Background,
+} from "reactflow";
+
+import "reactflow/dist/style.css";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
 export default function GraphDisplay() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: any) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
   return (
     <FlexCol
       style={{
@@ -11,6 +36,18 @@ export default function GraphDisplay() {
         border: "2px solid black",
         margin: "12px",
       }}
-    ></FlexCol>
+    >
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant="dots" gap={12} size={1} />
+      </ReactFlow>
+    </FlexCol>
   );
 }
