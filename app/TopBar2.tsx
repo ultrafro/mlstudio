ff1887514e70086571ddfac96de25799fe1a46b8
ultrafro <hisham.bedri@gmail.com>
2023-12-30 21:37:15 -0500

    update

--- app/TopBar2.tsx
+++ app/TopBar2.tsx
@@ -41,7 +41,7 @@ export default function TopBar2({
           </div> */}
           <Button
             src={"/icons/file.png"}
-            label="Templates"
+            label="Templats"
             imgSize={32}
             override="w-24 h-[75px]"
             onClick={openFile}
